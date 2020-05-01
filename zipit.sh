3a6ab403e921556041739724c545fe39d8b1bda6
1118146 <1118146l@ai914311.appsig.com>
2020-05-01 15:17:48 -0600

    Separation of bible into books to ease working with transcriptions and hopefully ease working with translation as well.

--- zipit.sh
+++ zipit.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 rm ase-kjv.zip
+cd usfx
+./prep.sh
+cd ..
 zip ase-kjv.zip usfx/BookNames.xml usfx/copr.htm usfx/ase-kjvmetadata.xml usfx/ase-kjv_usfx.xml usfx/ase-kjv-VernacularAdditional.xml usfx/ase-kjv-VernacularParms.xml usfx/latin.css
+rm usfx/ase-kjv_usfx.xml
 
