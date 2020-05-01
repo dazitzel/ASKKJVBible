d4b12947b3fabf30a116acb26e996941668b7449
1118146 <1118146l@ai914311.appsig.com>
2020-05-01 14:41:09 -0600

    Move the files from ase-kjv to usfx and rename the base usfx file to easier integrate with haiola. Also added signline commands to get base printing of the english text to be readable in a vertical evironment.

--- zipit.sh
+++ zipit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm ase-kjv.zip
-zip ase-kjv.zip ase-kjv/*
+zip ase-kjv.zip usfx/BookNames.xml usfx/copr.htm usfx/ase-kjvmetadata.xml usfx/ase-kjv_usfx.xml usfx/ase-kjv-VernacularAdditional.xml usfx/ase-kjv-VernacularParms.xml usfx/latin.css
 
